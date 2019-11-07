cec877f3ba9c70fae9a453d81b71647e18f9395c
ryydder <segalcsam@gmail.com>
2019-11-07 17:52:57 -0500

    landing page added

--- app.js
+++ app.js
@@ -2,6 +2,12 @@ var express = require("express");
 var app = express();
 var request = require("request");
 
-app.get("/landing",(req,res)=>{
+app.use(express.static("public"))
+
+app.get("/",(req,res)=>{
     res.render("landing.ejs")
-})
+});
+
+app.listen(3000,function(){
+    console.log("serving 3000");
+});
